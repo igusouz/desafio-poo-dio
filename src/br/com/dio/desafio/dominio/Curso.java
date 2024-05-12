515463e20a5a1fc28779ccecaeb16eb6c09514cd
igusouz <igorrafael.jobs@gmail.com>
2024-05-12 12:43:39 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,40 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String decricao;
+    private int CargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDecricao() {
+        return decricao;
+    }
+
+    public void setDecricao(String decricao) {
+        this.decricao = decricao;
+    }
+
+    public int getCargaHoraria() {
+        return CargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        CargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", decricao='" + decricao + '\'' +
+                ", CargaHoraria=" + CargaHoraria +
+                '}';
+    }
+}
